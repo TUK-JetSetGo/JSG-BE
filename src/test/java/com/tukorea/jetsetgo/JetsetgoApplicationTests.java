8f67fe1e67326d5988f6ad8559184a4d68b426fe
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-01-13 23:42:47 +0900

    chore : 프로젝트 세팅

---
+++ src/test/java/com/tukorea/jetsetgo/JetsetgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tukorea.jetsetgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetsetgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
