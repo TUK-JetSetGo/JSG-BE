31ecd242ec976f2f2d4110f74ca1b771bae5ff1c
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-02-16 11:22:37 +0900

    fix: redis config 수정

--- src/main/java/org/tukcapstone/jetsetgo/global/config/RedisConfig.java
+++ src/main/java/org/tukcapstone/jetsetgo/global/config/RedisConfig.java
@@ -1,16 +1,30 @@
 package org.tukcapstone.jetsetgo.global.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Slf4j
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.host}")
+    private String host;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory();
+        log.info("Connecting to Redis at {}:{}", host, port);
+
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        return new LettuceConnectionFactory(configuration);
     }
 
     @Bean
